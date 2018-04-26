fc58a5404fd8d35c66f311fda62df51b0c8d4c89
Shafeeq Shaar <sean.spilot@gmail.com>
2018-04-26 03:28:28 +0300

    Updated to 5.6L

--- config/app.php
+++ config/app.php
@@ -1,21 +1,4 @@
 <?php
-/**
- * FIRSTSOLUTIONS / CLARIMOUNT CONFIDENTIAL
- * __________________
- *
- * 2017 FirstSolutions Association (https://firstsolu.com)
- * All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of FirstSolutions Association and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to FirstSolutions Associations
- * and its suppliers and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from FirstSolutions Association.
- */
 
 return [
 
@@ -69,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://clarimount'),
+    'url' => env('APP_URL', 'https://clarimount.tech'),
 
     /*
     |--------------------------------------------------------------------------
@@ -124,23 +107,6 @@ return [
     'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Logging Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure the log settings for your application. Out of
-    | the box, Laravel uses the Monolog PHP logging library. This gives
-    | you a variety of powerful log handlers / formatters to utilize.
-    |
-    | Available Settings: "single", "daily", "syslog", "errorlog"
-    |
-    */
-
-    'log' => env('APP_LOG', 'single'),
-
-    'log_level' => env('APP_LOG_LEVEL', 'debug'),
 
     /*
     |--------------------------------------------------------------------------
