23b77acebe2192310082486b3e6f787055b0c234
Shafeeq Shaar <sean.spilot@gmail.com>
2018-05-06 22:04:22 +0300

    Completed the requisition pdf form

--- resources/views/pdf/purchase-requisitions/form.blade.php
+++ resources/views/pdf/purchase-requisitions/form.blade.php
@@ -77,11 +77,11 @@
     <table class="pure-table pure-table-bordered tight-table">
         <colgroup>
             <col style='width:1%;'>
-            <col style='width:80%;'>
+            <col style='width:99%;'>
         </colgroup>
         <thead>
             <tr style="background-color:black;">
-                <th style="color:white;" class="center">S.NO</th>
+                <th style="color:white;" class="center">S.NO.</th>
                 <th style="color:white;" class="center">ITEM DESCRIPTION</th>
                 <th style="color:white;" class="center">QUANTITY</th>
             </tr>
@@ -113,21 +113,111 @@
     {{-- Signatures of requests --}}
     <table class="pure-table tight-table" style="margin-top:20px;">
         <colgroupd>
-            <col style="width:50%;">
+            <col style="width:33%;">
+            <col style="width:33%;">
         </colgroupd>
     	<tbody>
             <tr>
-                <td class="narrow"><strong>CHECKED AND REGISTERED BY</strong></td>
-                <td class="narrow"><strong>VERIFIED AND APPROVED BY</strong> (Head of IT Assets Management)</td>
+                <td class="narrow"><strong>REQUESTED BY<br>NAME</strong></td>
+                <td class="narrow"><strong>RECOMMENDED BY<br>NAME</strong></td>
+                <td class="narrow"><strong>APPROVED BY</strong> (Having Financial Authority)<br><strong>NAME</strong></td>
             </tr>
             <tr style="height:80px;">
-                <td></td><td></td>
+                <td></td>
+                <td></td>
+                <td></td>
             </tr>
             <tr>
+                <td><strong>SIGNATURE</strong></td>
                 <td><strong>SIGNATURE</strong></td>
                 <td><strong>SIGNATURE</strong></td>
             </tr>
     	</tbody>
+    </table>
+</div>
+
+<div style="padding-top:15px;">
+    <table class="pure-table pure-table-bordered tight-table">
+        <colgroup>
+            <col style='width:1%;'>
+            <col style="width:99%">
+        </colgroup>
+        <thead>
+        <tr style="background-color:black;">
+            <th style="color:white;" class="center">S.NO.</th>
+            <th style="color:white;" class="center">TO BE FILLED ONLY BY IT ASSETS MANAGEMENT SECTION <br/> ITEM DESCRIPTION</th>
+            <th style="color:white;" class="center">QUANTITY</th>
+        </tr>
+        </thead>
+        <tbody>
+            @foreach($data->items as $counter => $item)
+                <tr>
+                    <td class="center">{{ ++$counter }}</td>
+                    <td>{{ $item->name }}</td>
+                    <td class="center">{{ $item->qty }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    {{-- Remarks if any --}}
+    <table class="pure-table pure-table-bordered tight-table">
+        <colgrop>
+            <col style="width:15%;">
+        </colgrop>
+        <tbody>
+        <tr>
+            <td><b>Remarks (if any)</b></td>
+            <td></td>
+        </tr>
+        </tbody>
+    </table>
+
+    {{-- Signatures of requests --}}
+    <table class="pure-table tight-table" style="margin-top:20px;">
+        <colgroupd>
+            <col style="width:50%;">
+        </colgroupd>
+        <tbody>
+        <tr>
+            <td class="narrow"><strong>CHECKED AND REGISTERED BY</strong></td>
+            <td class="narrow"><strong>VERIFIED AND APPROVED BY</strong> (Head of IT Assets Management)</td>
+        </tr>
+        <tr style="height:50px;">
+            <td></td><td></td>
+        </tr>
+        <tr>
+            <td><strong>SIGNATURE</strong></td>
+            <td><strong>SIGNATURE</strong></td>
+        </tr>
+        </tbody>
+    </table>
+</div>
+
+<div style="padding-top:15px;">
+    <table class="pure-table tight-table">
+        <colgroupd>
+            <col style="width:50%;">
+        </colgroupd>
+        <thead>
+            <tr style="background-color:black;">
+                <th style="color:white;" colspan="2">TO BE USED BY PREMISES &amp; ADMIN ONLY.</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td class="narrow"><strong>Purchase Order Prepared By</strong></td>
+                <td class="narrow"><strong>IT Purchasing Head</strong></td>
+            </tr>
+                <tr style="height:50px;">
+                    <td></td>
+                    <td></td>
+                </tr>
+            <tr>
+                <td><strong>SIGNATURE</strong></td>
+                <td><strong>SIGNATURE</strong></td>
+            </tr>
+        </tbody>
     </table>
 </div>
 
