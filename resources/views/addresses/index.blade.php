5e1c8175f1973ecf471fe233be22acfc9ba53436
Shafeeq Shaar <sean.spilot@gmail.com>
2018-05-10 10:10:49 +0300

    Add addresses

---
+++ resources/views/addresses/index.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app', ['title' => trans('words.suppliers')])
+
+@section('header')
+	<nav class="breadcrumb" aria-label="breadcrumbs">
+		<ul>
+			<li>
+				<a href="{{ route('dashboard.index') }}" aria-current="page">
+					<span class="icon is-small"><i class="fa fa-inbox"></i></span>
+					<span>{{ trans('words.dashboard') }}</span>
+				</a>
+			</li>
+			<li class="is-active">
+				<a href="{{ route('vendors.index') }}">
+					<span class="icon is-small"><i class="fa fa-truck"></i></span>
+					<span>{{ trans('words.suppliers') }}</span>
+				</a>
+			</li>
+		</ul>
+	</nav>
+@endsection
+
+@section('content')
+
+	<div class="columns">
+		<div class="column is-4">
+			<p class="title is-6">
+				<b>{{ trans('words.inactive') }} {{ trans('words.supplier') }}</b>
+			</p>
+
+			<div class="notification is-warning">
+				<p class="subtitle is-7">
+					<b>{{ $inactiveVendors }}</b>
+				</p>
+			</div>
+		</div>
+
+		<div class="column is-4">
+			<p class="title is-6">
+				<b>{{ trans('words.active') }} {{ trans('words.supplier') }}</b>
+			</p>
+
+			<a href="{{ route('vendors.all') }}">
+				<div class="notification is-success">
+					<p class="subtitle is-7">
+						<b>{{ $activeVendors }}</b>
+					</p>
+				</div>
+			</a>
+		</div>
+	</div>
+
+	<vendors :can-create.number="{{ Auth::user()->can('create-vendor') ? '1' : '0' }}"></vendors>
+
+@endsection
