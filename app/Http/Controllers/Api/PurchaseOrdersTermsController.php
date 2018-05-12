cdb571c22fca79805a8114c3da9d9058dc2637e0
Shafeeq Shaar <sean.spilot@gmail.com>
2018-05-12 10:36:42 +0300

    Create POTerms controller to attach the terms to the PO

---
+++ app/Http/Controllers/Api/PurchaseOrdersTermsController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Model\PurchaseTerm;
+use App\Models\PurchaseOrder;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PurchaseOrdersTermsController extends Controller
+{
+    public function attach(Request $request)
+    {
+        $request->validate([
+            'purchase_order_id' => 'required|exists:purchase_orders,id',
+            'term_id' => 'required|exists:purchase_terms,id',
+        ]);
+
+        $po = PurchaseOrder::find($request->purchase_order_id);
+        $term = PurchaseTerm::find($request->term_id);
+
+        $po->terms()->attach($term);
+
+        return $po;
+    }
+}
