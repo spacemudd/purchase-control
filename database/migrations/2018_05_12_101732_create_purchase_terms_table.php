78e9a87bdeecf4add37b90b07529a0a92acc4e09
Shafeeq Shaar <sean.spilot@gmail.com>
2018-05-12 10:23:50 +0300

    Add purchase terms table

---
+++ database/migrations/2018_05_12_101732_create_purchase_terms_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchaseTermsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchase_terms', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('type_id')->unsigned()->nullable();
+            $table->foreign('type_id')->references('id')->on('purchase_terms_types');
+            $table->binary('value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchase_terms');
+    }
+}
