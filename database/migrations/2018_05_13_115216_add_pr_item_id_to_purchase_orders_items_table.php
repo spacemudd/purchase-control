119c75efc1fbadc05bafa4469214b0314ae2a809
Shafeeq Shaar <sean.spilot@gmail.com>
2018-05-13 11:53:41 +0300

    Database: pr_item_id FK on purchase_orders_items table

---
+++ database/migrations/2018_05_13_115216_add_pr_item_id_to_purchase_orders_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddPrItemIdToPurchaseOrdersItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('purchase_orders_items', function (Blueprint $table) {
+            $table->integer('pr_item_id')->unsigned();
+            $table->foreign('pr_item_id')->references('id')->on('purchase_requisition_items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('purchase_orders_items', function (Blueprint $table) {
+            $table->dropForeign(['pr_item_id']);
+            $table->dropColumn(['pr_item_id']);
+        });
+    }
+}
